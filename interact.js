a96235daa39fc8b5fe5146587cbeb0a6f9cc42d1
Hayley Lucas <halucas93@gmail.com>
2022-11-01 04:27:32 +1100

    landing page

---
+++ interact.js
@@ -0,0 +1,20 @@
+// $(document).ready(function(){
+//   $("#btn").click(function(){
+//     alert("clicked button");
+//   });
+// });
+
+// $(document).ready(msg(){
+//   $("#btn").click(msg(){
+//     alert("clicked button");
+//   });
+// });
+
+$("#btn").on("click",function(){
+  alert("clicked button");
+});
+
+
+// function msg() {
+//   alert("marvel fan");
+// }
